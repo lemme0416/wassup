12a2971a5927e57714518b26860627106dc094b0
s106062131 <aaa9471@gmail.com>
2019-06-06 00:29:55 +0800

    modify and forget pw v1.5

--- wass_up/forgetpw.php
+++ wass_up/forgetpw.php
@@ -3,8 +3,8 @@ session_start();
 ?>
 <body background = "pw.jpg">
 <form method="POST">
-ID:<input type = "text" name = "id"><br>
-security question: <input type = "text" name = "security"><br>
+ID:<input type = "text" name = "id" id = "id"><br>
+security question: <input type = "text" name = "security" id = "security"><br>
 <input type = "submit" value ="finish"><input type = "button" value = "return" onclick = "javascript:location.href='index.php'">
 </form>
 </body>
@@ -13,7 +13,7 @@ security question: <input type = "text" name = "security"><br>
     $dsn = 'mysql:host=localhost;dbname=wassup';
     $dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);   
     $sth = $dbh->prepare('select count(*) as r from users where id = ? and problem = ? ;');
-    $sth->execute(array($_POST['id'],$_POST['security']));
+    $sth->execute(array(@$_POST['id'],@$_POST['security']));
     $result = $sth->fetch(PDO::FETCH_ASSOC);
     if($result['r'] == 1 ){
         $_SESSION['login'] = $_POST['id'];
--- wass_up/modifypw.php
+++ wass_up/modifypw.php
@@ -10,16 +10,16 @@ enter new password again: <input type = "password" name = "npw2"><br>
 </body>
 <?php
     require_once('login.php');
-    $dsn = 'mysql:host=localhost;dbname=s106062131';
+    $dsn = 'mysql:host=localhost;dbname=wassup';
     $dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
     /*
     $sth = $dbh->prepare('select count(*) as r from users where id = ? and pw = ? ;');
     $sth->execute(array($_SESSION['login'],$_POST['opw']));
     $result = $sth->fetch(PDO::FETCH_ASSOC);
     */
-    if($_POST['npw'] == $_POST['npw2']){
+    if(@$_POST['npw'] == @$_POST['npw2']){
         $sth = $dbh->prepare('update  users set pw = ? where id = ?');
-        $sth->execute(array($_POST['npw'],$_SESSION['login']));
+        $sth->execute(array(@$_POST['npw'],$_SESSION['login']));
     }
     else echo "<script>alert('兩次密碼不一樣')</script>";
     
