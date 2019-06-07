d34b90adfd1902be1be920fc65a14476a86b8b8d
s106062131 <aaa9471@gmail.com>
2019-06-08 00:20:01 +0800

    music v2.1

--- wass_up/music.php
+++ wass_up/music.php
@@ -1,14 +1,21 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
-    $obj = json_decode($_GET["x"], false);
-    require_once('login.php');
-    $dsn = 'mysql:host=localhost;dbname=wassup';
-    $dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
-    $sth = $dbh->prepare('select music from music order by id;');
-    $sth->bind_param("sss");
-    $sth->execute();
-    $result = $sth->get_result();
-    $outp = $result->fetch_all(MYSQLI_ASSOC);
-    echo json_encode($outp);
-?>
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_GET["x"], false);
+require_once('login.php');
+$dsn = 'mysql:host=localhost;dbname=wassup';
+$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
+$sth = $dbh->prepare('select name from music order by id;');
+//$sth->bindparam("ss");
+$sth->execute();
+/*
+while ($result = $sth->fetch()){
+    echo json_encode($result);
+}
+*/
+$result = $sth->fetchAll();
+//$outp = $result->fetch_all(MYSQLI_ASSOC);
 
+//echo json_encode($result);
+echo json_encode($result);
+
+?>
