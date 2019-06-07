0cae855532548827904bb6433471209464a88338
Chen Huang <huangchen1999@gmail.com>
2019-06-07 20:25:39 +0800

    color

--- wass_up/left.php
+++ wass_up/left.php
@@ -18,22 +18,22 @@
 	</style>
 </head>
 <body>
-	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump()">
+	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump("yellow")">
 		<p>修改密碼</p>
 	</div>
-	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump()">
+	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump("blue")">
 		<p>上傳音樂</p>
 	</div>
-	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump()">
+	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump("red")">
 		<p>排行榜</p>
 	</div>
-	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump()">
+	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump("green")">
 		<p>list1</p>
 	</div>
-	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump()">
+	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump("orange")">
 		<p>list2</p>
 	</div>
-	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump()">
+	<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump("purple")">
 		<p>list3</p>
 	</div>
 
@@ -49,8 +49,8 @@
 		function color_shallow(x) {
 			x.style.backgroundColor = 'gray';
 		}
-		function jump(){
-			var address = "right.php?text=123";
+		function jump(x){
+			var address = x + ".html";
 			parent.frames[1].location = address;
 		}
     </script>
