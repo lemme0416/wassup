29745b331d69e91104cbcb03df9fc35e205286ff
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-07 22:26:15 +0800

    song

--- wass_up/red.php
+++ wass_up/red.php
@@ -30,9 +30,11 @@
 	while($row=$sth->fetch(PDO::FETCH_ASSOC)){
 		$song_name = $row['name'];
 		$song_string = (string)$song_name;
-		echo '<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump('.$song_string.')">';
-			echo"<p>$song_name</p>";
-		echo"</div>";
+		echo '<div onmouseover="color_deep(this)" onmouseout="color_shallow(this)" onclick="jump('."'$song_string'".')">';
+		echo"	<p>$song_name</p>
+		";
+		echo"</div>
+		";
 	}
 ?>
 <script>
