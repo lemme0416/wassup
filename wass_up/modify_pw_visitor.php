6c053aa6efeab10d65b44d771876a30919355b30
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-09 00:14:48 +0800

    modify_pw_visitor v2.7

--- wass_up/modify_pw_visitor.php
+++ wass_up/modify_pw_visitor.php
@@ -1,26 +1,57 @@
 <?php
-    header("Content-Type: text/html; charset=utf-8");
-    session_start();
+session_start();
 ?>
-<body background = "pw.jpg">
-    <form method="POST">
-        new password:<input type = "password" name = "npw"><br>
-        enter new password again: <input type = "password" name = "npw2"><br>
-        <input type = "submit" value ="修改密碼" require = "true" onclick="javascript:location.href='index.php'">
-    </form>
-</body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Modify! | WassUp!</title>
+        <link rel="shortcut icon" href="https://imgur.com/G4KMHP3.png" type="image/x-icon" />
+        
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+        
+        <link rel="stylesheet" href="css/modify_pw.css">
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>        
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>    
+    </head>
+    <body>
+        <ul class="text-animation hidden">
+            <li>M</li>
+            <li>o</li>
+            <li>d</li>
+            <li>i</li>
+            <li>f</li>
+            <li>y</li>
+            <li>!</li>
+        </ul>
+        <div class="box">        
+            <form method="POST">
+                <input type = "password" name = "npw" placeholder="New Password"><br>
+                <input type = "password" name = "npw2" placeholder="Enter New Password Again"><br>
+                <input type = "submit" value ="Confirm" require = "true">
+                <input type="button" value="HomePage" onclick = "javascript:location.href='index.php'">
+            </form>
+        </div>
+        <script type="text/javascript">
+            $(function(){
+                setTimeout(function(){
+                    $('.text-animation').removeClass('hidden');
+                }, 500);
+            });
+        </script>
+    </body>
+</html>
 <?php
     require_once('login.php');
     $dsn = 'mysql:host=localhost;dbname=wassup';
     $dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
-    if(@$_POST['npw']!='' && @$_POST['npw2']!= ''){
-        if(@$_POST['npw'] == @$_POST['npw2']){
-            $sth = $dbh->prepare('update  users set pw = ? where id = ?');
-            $sth->execute(array(@$_POST['npw'],$_SESSION['login']));
-            echo "<script>alert('Change!')</script>";
-        }
-        else echo "<script>alert('兩次密碼不一樣')</script>";        
+    if(@$_POST['npw'] == @$_POST['npw2']){
+        $sth = $dbh->prepare('update  users set pw = ? where id = ?');
+        $sth->execute(array(@$_POST['npw'],$_SESSION['login']));
     }
-
+    else echo "<script>alert('兩次密碼不一樣')</script>";
     
 ?> 
