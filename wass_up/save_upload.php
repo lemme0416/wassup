fe6ff03a9709149a55f63c8376e678bba62a4017
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-08 22:38:53 +0800

    upload music v3.6

--- wass_up/save_upload.php
+++ wass_up/save_upload.php
@@ -8,22 +8,25 @@
 		if($_FILES['uploaded_file']['type']!='audio/mp3'){
 			header("Location: upload.php?value=wrong_type"); 
 		}
-		$path = "music/";
-		$des = $path . basename( $_FILES['uploaded_file']['name']);
-		if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $des)) {
-			$return_value = 'success';
-			$file_name = basename( $_FILES['uploaded_file']['name'], '.mp3');
-			require_once('login.php');
-			$dsn = 'mysql:host=localhost;dbname=wassup';
-			$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
-			$sth=$dbh->prepare('insert into music(name,music) values(:name,:music)');
-			$sth->bindParam(':name', $file_name);
-			$sth->bindParam(':music',$file_name);
-			$sth->execute();
-		} else{
-			$return_value = 'error';
+		else{
+			$path = "music/";
+			$des = $path . basename( $_FILES['uploaded_file']['name']);
+			if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $des)) {
+				$return_value = 'success';
+				$file_name = basename( $_FILES['uploaded_file']['name'], '.mp3');
+				require_once('login.php');
+				$dsn = 'mysql:host=localhost;dbname=wassup';
+				$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
+				$sth=$dbh->prepare('insert into music(name,music) values(:name,:music)');
+				$sth->bindParam(':name', $file_name);
+				$sth->bindParam(':music',$file_name);
+				$sth->execute();
+			} else{
+				$return_value = 'error';
+			}
+			$inst = "Location: upload.php?value=".$return_value;
+			header($inst); 			
 		}
-		$inst = "Location: upload.php?value=".$return_value;
-		header($inst); 
+
 	}
 ?>
