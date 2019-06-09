8cb2634d25c5a9d4b8e57b0fc16f6e32099069e8
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-09 17:32:13 +0800

    search css

--- wass_up/search.php
+++ wass_up/search.php
@@ -5,6 +5,9 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
         
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>        
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        
         <link rel="stylesheet" href="css/search.css">
 
     </head> 
