7496bf9118d4070ec486d1bc4b776f562eab7c17
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-09 16:55:29 +0800

    search css v2.8.4

--- wass_up/home.php
+++ wass_up/home.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['login'])=='') {
         <link rel="shortcut icon" href="https://imgur.com/G4KMHP3.png" type="image/x-icon" />
 
     </head>
-    <frameset rows="8*,80*,10*" border="1" bordercolor=black>
+    <frameset rows="8*,80*,10*" border="0" bordercolor=black>
         <frame src="search.php" name="top">
         <frameset cols="1*,5*">
             <frame src="left.php" name="left">
