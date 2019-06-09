c6ea92f2a099c46ae0bcf64804fea05bd7103b5e
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-09 11:47:57 +0800

    frameset v1

--- wass_up/home.php
+++ wass_up/home.php
@@ -11,15 +11,30 @@ if (isset($_SESSION['login'])=='') {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <title>Home | WassUp!</title>
-</head>
-<frameset rows="6*,1*">
-    <frameset cols="1*,5*">
-        <frame src="left.php" name="left">
-        <frame src="yellow.php" name="right">
-    </frameset>
-    <frame src="music.html" name="music"></frame>
-</frameset>
+    <head>
+        <meta charset="utf-8" />
+        <title>Home | WassUp!</title>
+        <link rel="shortcut icon" href="https://imgur.com/G4KMHP3.png" type="image/x-icon" />
+        
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+        
+        <!-- <link rel="stylesheet" href="css/index.css"> -->
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>        
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        
+        <!-- <script src="js/index.js"></script> -->
+    </head>
+    <body>
+        <frameset rows="7*,1*">
+            <frameset cols="1*,5*">
+                <frame src="left.php" name="left" frameborder="0">
+                <frame src="yellow.php" name="right" frameborder="0">
+            </frameset>
+            <frame src="music.html" name="music" noresize="noresize" frameborder="0"></frame>
+        </frameset>
+    </body>
 </html>
