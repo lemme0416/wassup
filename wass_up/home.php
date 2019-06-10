13d0622adc62332f85ac0b4aa7587ca81f4a5dcf
s106062131 <aaa9471@gmail.com>
2019-06-11 00:13:37 +0800

    music v3.4

--- wass_up/home.php
+++ wass_up/home.php
@@ -24,6 +24,6 @@ if (isset($_SESSION['login'])=='') {
             <frame src="left.php" name="left">
             <frame src="yellow.php" name="right">
         </frameset>
-        <frame src="music.html" name="music" >
+        <frame src="music.php" name="music" >
     </frameset>
 </html>
