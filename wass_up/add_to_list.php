fe1d1dc422c504a710c1bcef6e80ad0886862bf1
Chen Huang <huangchen1999@gmail.com>
2019-06-11 01:06:35 +0800

    list.php

--- wass_up/add_to_list.php
+++ wass_up/add_to_list.php
@@ -5,7 +5,8 @@
 		$dsn = 'mysql:host=localhost;dbname=wassup';
 		$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
 		$song_name = $_GET['song_name'];
-		$inst = 'insert into '.$_SESSION['login'].'_list_'.$_POST['list_name'].' values('."'$song_name'".');';
+		$song_id = $_GET['song_id'];
+		$inst = 'insert into '.$_SESSION['login'].'_list_'.$_POST['list_name'].' values('.$song_id.','."'$song_name'".');';
 		$sth = $dbh->prepare($inst);
 		$sth->execute();
 		header("Location: red.php");
