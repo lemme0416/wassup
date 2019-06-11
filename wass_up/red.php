91e92ac5a39ce74b651e01eb0209e25fca714892
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 00:06:49 +0800

    play button css v1.3

--- wass_up/red.php
+++ wass_up/red.php
@@ -36,7 +36,7 @@
 		$song_id = $row['id'];
 		echo '<div>
 		';
-		echo '<img src="https://i.imgur.com/6EMbicT.png" onmouseover="play_black(this)" onmouseout="play_white(this)" onclick="jump('."'$song_id'".')">';
+		echo '<img src="https://i.imgur.com/CY4HN75.png" onmouseover="play_black(this)" onmouseout="play_white(this)" onclick="jump('."'$song_id'".')">';
 		echo '<p>'.$song_name.'</p>';
 		echo '<form method="POST" onclick="bubble(event)" id="'.$song_id.'" action="add_to_list.php?song_name='.$song_name.'&song_id='.$song_id.'"><input type="submit" value="add to list"></form>';
 		echo '<select name="list_name" onclick="bubble(event)" form="'.$song_id.'">';
@@ -48,17 +48,11 @@
 	}
 ?>
 <script>
-	function color_deep(x) {
-		x.style.backgroundColor = 'black';
-	}
-	function color_shallow(x) {
-		x.style.backgroundColor = 'DarkSlateGray ';
-	}
 	function play_black(x){
-		x.src="https://i.imgur.com/HiPrjmk.png";
+		x.src="https://i.imgur.com/POXP1rs.png";
 	}
 	function play_white(x){
-		x.src="https://i.imgur.com/6EMbicT.png;"
+		x.src="https://i.imgur.com/CY4HN75.png";
 	}
 	function jump(x){
 		var address = "music2.php?id=" + x;
