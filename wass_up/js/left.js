8923a7ed853f9d973878e2f6ab9d047b2c47bd2b
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-11 17:16:11 +0800

    upload css v2.4.3

--- wass_up/js/left.js
+++ wass_up/js/left.js
@@ -4,13 +4,3 @@ function color_deep(x) {
 function jump(x){
     parent.frames[2].location = x;
 }
-
-var nono = document.getElementById('modal-wrapper');
-window.onclick = function(event) {
-    if (event.target == nono) {
-        nono.style.display = 'none';
-        console.log("QAQ");
-    }
-    console.log("AAA");
-
-}
