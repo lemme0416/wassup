84ad5181488af3435dffe7d00cabbf5149e42e82
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-11 16:28:19 +0800

    upload css v2.9.2

--- wass_up/js/left.js
+++ wass_up/js/left.js
@@ -9,6 +9,8 @@ function jump(x){
 //     hid.hidden = !(hid.hidden);
 // }
 
+// If user clicks anywhere outside of the modal, Modal will close
+
 var modal = document.getElementById('modal-wrapper');
 window.onclick = function(event) {
     if (event.target == modal) {
