acf634c9f3a1bbeded0c49e8f1daee032a0ae58b
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-11 16:46:27 +0800

    upload css v2.9.8

--- wass_up/js/left.js
+++ wass_up/js/left.js
@@ -4,12 +4,6 @@ function color_deep(x) {
 function jump(x){
     parent.frames[2].location = x;
 }
-// function show_form(){
-//     var hid = document.getElementById("hidden_form");
-//     hid.hidden = !(hid.hidden);
-// }
-
-// If user clicks anywhere outside of the modal, Modal will close
 
 var modal = document.getElementById('modal-wrapper');
 window.onclick = function(event) {
