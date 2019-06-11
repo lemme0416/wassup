f3b23ec33e3e18891747281fb36ccb516fdade8f
Chen Huang <huangchen1999@gmail.com>
2019-06-11 22:33:42 +0800

    test

--- wass_up/delete_list.php
+++ wass_up/delete_list.php
@@ -10,7 +10,7 @@
 			$dsn = 'mysql:host=localhost;dbname=wassup';
 			$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
 			$inst = 'DROP TABLE '.$_SESSION['login'].'_list_'.$list_name.';';
-			echo $_GET['list_name'];
+			echo 'fuck';
 			$sth = $dbh->prepare($inst);
 			$sth->execute();
 			$inst = 'delete from '.$_SESSION['login'].'_list where list_name='."'$list_name'".';';
