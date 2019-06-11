8b494be4b8f8f5d8962c9a7b555703435e09033f
Chen Huang <huangchen1999@gmail.com>
2019-06-11 15:13:51 +0800

    fix

--- wass_up/remove_from_list.php
+++ wass_up/remove_from_list.php
@@ -6,7 +6,7 @@
 		$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
 		$list_name = $_GET['list_name'];
 		$song_id = $_GET['song_id'];
-		$inst = 'delete from '.$_SESSION['login'].'_list_'.$list_name.' where id='.$song_id ;
+		$inst = 'delete from '.$_SESSION['login'].'_list_'.$list_name.' where id='.$song_id .';';
 		$sth = $dbh->prepare($inst);
 		$sth->execute();
 		$destination = 'Location: list.php?list_name='.$list_name;
