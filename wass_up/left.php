dd1f32178106dd6de7995fa691e9443866647050
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-11 18:29:55 +0800

    upload css v2.5.3

--- wass_up/left.php
+++ wass_up/left.php
@@ -45,7 +45,7 @@ session_start();
 
 	<div id="modal-wrapper" class="modal">
 		<form class="modal-content animate" method = "POST" action="add_list.php" >
-			<p onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close PopUp">&times;</p>
+			<p onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close PopUp">x</p>
 			<input type="text" required="true" name="list_name"><br>
 			<input type="submit" value="新增">
 		</form>
