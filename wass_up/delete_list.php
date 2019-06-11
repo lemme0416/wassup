e841149b0652ccdbeabd745b47f406e5491ce82f
Chen Huang <huangchen1999@gmail.com>
2019-06-11 17:32:51 +0800

    fix

--- wass_up/delete_list.php
+++ wass_up/delete_list.php
@@ -9,7 +9,7 @@
 			require_once('login.php');
 			$dsn = 'mysql:host=localhost;dbname=wassup';
 			$dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
-			$inst = 'drop table '.$_SESSION['login'].'_list_'.$list_name;
+			$inst = 'drop table '.$_SESSION['login'].'_list_'.$list_name.';';
 			$sth = $dbh->prepare($inst);
 			$sth->execute();
 			$inst = 'delete from '.$_SESSION['login'].'_list where list_name='."'$list_name'".';';
