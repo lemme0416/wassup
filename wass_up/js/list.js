95b3604072b8f0ade4c9933eed57231ff34f01de
Chen Huang <huangchen1999@gmail.com>
2019-06-12 18:52:34 +0800

    ..

--- wass_up/js/list.js
+++ wass_up/js/list.js
@@ -5,7 +5,7 @@ function play_white(x){
     x.src="https://i.imgur.com/T1iuPh7.png";
 }
 function jump(id, table){
-    var obj = {"table":table, "id":id-1};
+    var obj = {"table":table, "id":id};
     parent.frames[3].play_music(obj);
 }
 function bubble(event){
--- wass_up/js/red.js
+++ wass_up/js/red.js
@@ -4,9 +4,8 @@ function play_black(x){
 function play_white(x){
     x.src="https://i.imgur.com/T1iuPh7.png";
 }
-function jump(x){
-    var address = "music2.php?id=" + x;
-    var obj = {"table":"music", "id":x};
+function jump(id, table){
+    var obj = {"table":table, "id":id};
     parent.frames[3].play_music(obj);
 }
 function bubble(event){
