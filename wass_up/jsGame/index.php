ebdf68fc208cc9a1825b8b445af0320d054df518
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 21:19:01 +0800

    js game css v2.2

--- wass_up/jsGame/index.php
+++ wass_up/jsGame/index.php
@@ -5,7 +5,7 @@
             
             <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
             
-            <link rel="stylesheet" href="css/index.css">
+            <link rel="stylesheet" href="index.css">
             
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1">
