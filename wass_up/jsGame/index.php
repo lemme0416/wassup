b2574ce145f133e644590da15071b2194a9863ac
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 21:56:51 +0800

    js game css v2.5.6

--- wass_up/jsGame/index.php
+++ wass_up/jsGame/index.php
@@ -50,8 +50,8 @@ var myGameArea = {
         this.title.appendChild(node);
         document.body.insertBefore(this.title, document.body.childNodes[0]);*/
 
-        this.canvas.width = 500;
-        this.canvas.height = 100%;
+        this.canvas.width = 600;
+        this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
