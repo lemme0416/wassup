89a0f4ccf53b82aaddfeae976d686a42fd160090
Chen Huang <huangchen1999@gmail.com>
2019-06-13 02:02:57 +0800

    little bug

--- wass_up/delete_list.php
+++ wass_up/delete_list.php
@@ -20,7 +20,7 @@
 			//重新整理左方頁面，使更新資料能即時顯示
 			echo "<script> parent.frames[1].location.reload(true) </script>";
 		}
-		//回到排行榜
-		echo "<script> window.location='red.php' </script>";
+		//回到亞洲排行榜
+		echo "<script> window.location='red.php?language=1' </script>";
 	}
 ?>
