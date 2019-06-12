56aecf0183f79bffa317c1a81ee1186f8bf96440
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 21:29:56 +0800

    js game css v2.4.5

--- wass_up/jsGame/index.php
+++ wass_up/jsGame/index.php
@@ -1,6 +1,16 @@
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="https://imgur.com/G4KMHP3.png" type="image/x-icon" />
+    
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    
+    <link rel="stylesheet" href="index.css">
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>        
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>   
 <style>
 body{
     text-align: center;
