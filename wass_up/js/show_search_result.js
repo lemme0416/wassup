7d9ff5ece17f394196a57a97c04253ea67af60d5
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 19:18:39 +0800

    add htmlentities v2.2

---
+++ wass_up/js/show_search_result.js
@@ -0,0 +1,13 @@
+function play_black(x){
+    x.src="https://i.imgur.com/P3DW52n.png";
+}
+function play_white(x){
+    x.src="https://i.imgur.com/T1iuPh7.png";
+}
+function jump(id, table){
+    var obj = {"table":table, "id":id};
+    parent.frames[3].play_music(obj);
+}
+function bubble(event){
+    event.cancelBubble = true;
+}
