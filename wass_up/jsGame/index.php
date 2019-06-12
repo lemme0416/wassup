76183dbf78053674ca9c35848975a39054e202dc
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 21:31:17 +0800

    js game css v2.4.7

--- wass_up/jsGame/index.php
+++ wass_up/jsGame/index.php
@@ -11,14 +11,6 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>        
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>   
-<style>
-canvas {
-    border:1px solid #d3d3d3;
-    background-color: #f1f1f1;
-    display: block;
-    margin: 0 auto;
-}
-</style>
 </head>
 <body onload="startGame()">
 <script>
