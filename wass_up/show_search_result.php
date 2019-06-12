5153a80edd96a5b6f1955344df73cec0519397ef
Wei-Ze Yuan <880416914andy@gmail.com>
2019-06-12 20:15:53 +0800

    show search result v1.6

--- wass_up/show_search_result.php
+++ wass_up/show_search_result.php
@@ -38,25 +38,29 @@
             while($row = $sth->fetch()){
                 $song_name = $row->name;
                 $song_id = $row->id;
-                echo '<div>
-                ';
-                echo '<img src="https://i.imgur.com/T1iuPh7.png" onmouseover="play_black(this)" onmouseout="play_white(this)" onclick="jump('.$song_id.','."'music'".')">';
-                echo '<p>'.htmlentities($song_name).'</p>';
-                echo '<form method="POST" onclick="bubble(event)" id="'.$song_id.'" action="add_to_list.php?song_name='.htmlentities($song_name).'&song_id='.$song_id.'"><input type="submit" value="+" title="Add to the list"></form>';
-                echo '<select name="list_name" onclick="bubble(event)" form="'.$song_id.'">';
-                echo '<option value="0">Choose a list</option>';
-                foreach($arr as $value){
-                    $html_value = htmlentities($value);
-                    echo '<option value='."'$html_value'".'>'.$html_value.'</option>';
+                if(empty($row) == false){
+                    echo '<div>
+                    ';
+                    echo '<img src="https://i.imgur.com/T1iuPh7.png" onmouseover="play_black(this)" onmouseout="play_white(this)" onclick="jump('.$song_id.','."'music'".')">';
+                    echo '<p>'.htmlentities($song_name).'</p>';
+                    echo '<form method="POST" onclick="bubble(event)" id="'.$song_id.'" action="add_to_list.php?song_name='.htmlentities($song_name).'&song_id='.$song_id.'"><input type="submit" value="+" title="Add to the list"></form>';
+                    echo '<select name="list_name" onclick="bubble(event)" form="'.$song_id.'">';
+                    echo '<option value="0">Choose a list</option>';
+                    foreach($arr as $value){
+                        $html_value = htmlentities($value);
+                        echo '<option value='."'$html_value'".'>'.$html_value.'</option>';
+                    }
+                    echo '</select>';
+                    echo '</div>';   
                 }
-                echo '</select>';
-                echo '</div>';         
+                else{
+                    echo '<h2>No Result!</h2>';
+                }
+      
             ?>
         <?php     
             }
         }
-        else echo "<h2>name doesnt exist</h2>";
-
     ?> 
     </body>
 </html>
