4a771fc46b29d65f1dcba3cd6ad2b7d9b71dea22
s106062131 <aaa9471@gmail.com>
2019-06-13 03:45:11 +0800

    home annotation

--- wass_up/home.php
+++ wass_up/home.php
@@ -1,6 +1,6 @@
 ﻿<?php
 session_start();
-
+//登入資料庫
 require_once('login.php');
 $dsn = 'mysql:host=localhost;dbname=wassup';
 $dbh = new PDO($dsn,$CFG['username'],$CFG['pw']);
@@ -17,13 +17,16 @@ if (isset($_SESSION['login'])=='') {
         <link rel="shortcut icon" href="https://imgur.com/G4KMHP3.png" type="image/x-icon" />
 
     </head>
+    <!-- 分成上下視窗 -->
     <frameset rows="8*,80*,9*" border="2" bordercolor=#240b36>
-        
+        <!-- 最上面的視窗 -->
         <frame src="search.php" name="top" noresize="noresize">
+        <!-- 中間分成左右兩個視窗 -->
         <frameset cols="12*,50*">
             <frame src="left.php" name="left">
             <frame src="jsGame/index.php" name="right">
         </frameset>
+        <!-- player -->
         <frame src="music.html" name="music" noresize="noresize">
     </frameset>
 </html>
